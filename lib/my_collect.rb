fc47f0ba7a9c5baa5a3871b366d0c4822f743684
Lakeisha McCree <lmccree215@icloud.com>
2020-02-03 23:58:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,5 +2,5 @@ def each(array)
 new_array = []
   i = 0
   while i > array.length
-    #new_array << yield array (i)
-    new_array.push(yield array [i]) 
+    #new_array << yield array(i)
+    new_array.push(yield array[i]) 
